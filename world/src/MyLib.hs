650be736f114e6f18d5027d43737f3ff891afb55
rizvan <rizvangaripov@gmail.com>
2023-12-16 18:06:42 +0300

    add converting data row to albums

--- world/src/MyLib.hs
+++ world/src/MyLib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module MyLib (someFunc) where
 
@@ -7,14 +8,16 @@ import Data.Csv
 import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8)
 import Data.Vector (Vector, toList)
+import Control.Lens
+import Data.List (groupBy, sortBy)
 
 data DataRow = DataRow
-  { trackId :: String
-  , trackName :: String
-  , trackAlbumId :: String
-  , trackAlbumName :: String
-  , trackAlbumReleaseDate :: String
-  , trackDuration :: Int
+  { _trackId :: String
+  , _trackName :: String
+  , _trackAlbumId :: String
+  , _trackAlbumName :: String
+  , _trackAlbumReleaseDate :: String
+  , _trackDuration :: Int
   } deriving Show
 
 instance FromNamedRecord DataRow where
@@ -33,6 +36,41 @@ readCSVFile filePath = do
         Left err -> Left $ "Error decoding CSV: " ++ err
         Right (_, v) -> Right $ toList v
 
+data Track = Track
+  { _ident :: String
+  , _name :: String
+  , _duration :: Int
+  } deriving Show
+
+data Album = Album
+  { _albumId :: String
+  , _albumName :: String
+  , _albumReleaseDate :: String
+  , _albumTracks :: [Track]
+  } deriving Show
+
+makeLenses ''DataRow
+makeLenses ''Track
+makeLenses ''Album
+
+toTrack :: DataRow -> Track
+toTrack row = Track
+  { _ident = row ^. trackId
+  , _name = row ^. trackName
+  , _duration = row ^. trackDuration
+  }
+
+toAlbum :: [DataRow] -> Album
+toAlbum row = Album
+  { _albumId = (head row) ^. trackAlbumId
+  , _albumName = (head row) ^. trackAlbumName
+  , _albumReleaseDate = (head row) ^. trackAlbumReleaseDate
+  , _albumTracks = map (\a -> toTrack a) row
+  }
+
+toAlbums :: [DataRow] -> [Album]
+toAlbums row = map (\a -> toAlbum a) $ groupBy (\a b -> a ^. trackAlbumId == b ^. trackAlbumId) $ sortBy (\a b -> compare (a ^. trackAlbumId) (b ^. trackAlbumId)) row
+
 someFunc :: IO ()
 someFunc = do
     let csvFile = "spotify_songs.csv"
@@ -40,4 +78,5 @@ someFunc = do
     case result of
         Left err -> putStrLn $ "Error: " ++ err
         Right dataRows -> do
-            putStrLn $ "First DataRow: " ++ show (dataRows)
+            let albums = toAlbums dataRows
+            putStrLn $ "Albums: " ++ show albums
